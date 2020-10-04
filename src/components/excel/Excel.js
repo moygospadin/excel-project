916e15aa1b02f4298764445453a72c9c999efbf8
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-05 00:38:29 +0300

    feat: add html template

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,22 @@
+import { $ } from '@core/dom'
 export class Excel {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
-    this.options = options.components || []
+    this.components = options.components || []
+  }
+  getRoot() {
+    const $root = $.create('div', 'excel')
+
+    this.components.forEach((Component) => {
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.innerHTML = component.toHTML()
+      $root.append($el)
+    })
+
+    return $root
+  }
+  render() {
+    this.$el.append(this.getRoot())
   }
 }
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,1 +1,8 @@
-export class DOMListener {}
+export class DOMListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('No $root provided from DOMListener')
+    }
+    this.$root = $root
+  }
+}
