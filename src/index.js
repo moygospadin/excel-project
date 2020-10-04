79ef55bb9a229635d481a8f21525e57d2a09824e
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-04 23:13:22 +0300

    feat: add base structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import { Excel } from '@/components/excel/Excel'
 import './scss/index.scss'
 
-async function start() {
-  return await Promise.resolve('hekll')
-}
-
-start().then(console.log)
+const excel = new Excel('#app', {
+  components: [],
+})
+console.log('Excel', excel)
