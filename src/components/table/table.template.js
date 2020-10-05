75bc9e7c2706f36fd90b6d5bc412326bd9522744
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-06 01:03:47 +0300

    feat: make table generation

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,3 +1,42 @@
+const CODES = {
+  A: 65,
+  Z: 90,
+}
+
+function createCell() {
+  return `
+  <div class="cell" contenteditable></div>
+  `
+}
+
+function createCol(el) {
+  return `
+  <div class="column">
+    ${el}
+  </div>
+  `
+}
+
+function createRow(index, content) {
+  return `
+  <div class="row">
+   <div class="row-info">${index ? index : ''}</div>
+   <div class="row-data">${content}</div>
+  </div>`
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
 export function createTable(rowsCount = 20) {
-  return '<h1>Table</h1>'
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+  const cols = new Array(colsCount).fill('').map(toChar).map(createCol).join('')
+  rows.push(createRow(null, cols))
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount).fill('').map(createCell).join('')
+    rows.push(createRow(i + 1, cells))
+  }
+  return rows.join('')
 }
