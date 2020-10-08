079e74ba64c3aeb89165b52c61de52d61aec8362
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-08 17:14:01 +0300

    feat: add base route structure

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,10 +1,9 @@
-import {$} from '@core/dom'
-import {Emitter} from '@core/Emitter'
-import {StoreSubscriber} from '@core/StoreSubscriber'
+import { $ } from '@core/dom'
+import { Emitter } from '@core/Emitter'
+import { StoreSubscriber } from '@core/StoreSubscriber'
 
 export class Excel {
-  constructor(selector, options) {
-    this.$el = $(selector)
+  constructor(options) {
     this.components = options.components || []
     this.store = options.store
     this.emitter = new Emitter()
@@ -16,10 +15,10 @@ export class Excel {
 
     const componentOptions = {
       emitter: this.emitter,
-      store: this.store
+      store: this.store,
     }
 
-    this.components = this.components.map(Component => {
+    this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el, componentOptions)
       $el.html(component.toHTML())
@@ -30,15 +29,13 @@ export class Excel {
     return $root
   }
 
-  render() {
-    this.$el.append(this.getRoot())
-
+  init() {
     this.subscriber.subscribeComponents(this.components)
-    this.components.forEach(component => component.init())
+    this.components.forEach((component) => component.init())
   }
 
   destroy() {
     this.subscriber.unsubscribeFromStore()
-    this.components.forEach(component => component.destroy())
+    this.components.forEach((component) => component.destroy())
   }
 }
