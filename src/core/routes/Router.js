1ea43d21fe0fd42e3496bbd2e13ff9fc372bbbe6
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-09 16:40:07 +0300

    feat: implemeted DIP

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,39 +1,41 @@
-import { $ } from "../dom";
-import { ActiveRoute } from "./ActiveRoute";
+import { Loader } from '../../components/Loader'
+import { $ } from '../dom'
+import { ActiveRoute } from './ActiveRoute'
 export class Router {
   constructor(selector, routes) {
     if (!selector) {
-      throw new Error("Selector is required");
+      throw new Error('Selector is required')
     }
 
-    this.$placeholder = $(selector);
-    this.routes = routes;
-    this.page = null;
-
-    this.changePageHandler = this.changePageHandler.bind(this);
-    this.init();
+    this.$placeholder = $(selector)
+    this.routes = routes
+    this.page = null
+    this.loader = new Loader()
+    this.changePageHandler = this.changePageHandler.bind(this)
+    this.init()
   }
   init() {
-    window.addEventListener("hashchange", this.changePageHandler);
-    this.changePageHandler();
+    window.addEventListener('hashchange', this.changePageHandler)
+    this.changePageHandler()
   }
-  changePageHandler() {
+  async changePageHandler() {
     if (this.page) {
-      this.page.destroy();
+      this.page.destroy()
     }
-    this.$placeholder.clear();
+    this.$placeholder.clear().append(this.loader)
 
-    const Page = ActiveRoute.path.includes("excel")
+    const Page = ActiveRoute.path.includes('excel')
       ? this.routes.excel
-      : this.routes.dashboard;
+      : this.routes.dashboard
 
-    this.page = new Page(ActiveRoute.param);
+    this.page = new Page(ActiveRoute.param)
+    const root = await this.page.getRoot()
 
-    this.$placeholder.append(this.page.getRoot());
+    this.$placeholder.clear().append(root)
 
-    this.page.afterRender();
+    this.page.afterRender()
   }
   destroy() {
-    window.removeEventListener("hashchange", this.changePageHandler);
+    window.removeEventListener('hashchange', this.changePageHandler)
   }
 }
--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,38 +1,78 @@
-import { Page } from "@core/Page";
-import { Excel } from "@/components/excel/Excel";
-import { Header } from "@/components/header/Header";
-import { Formula } from "@/components/formula/Formula";
-import { Table } from "@/components/table/Table";
-import { Toolbar } from "@/components/toolbar/Toolbar";
-import { createStore } from "@core/store/createStore";
-import { rootReducer } from "@/redux/rootReducer";
-import { storage, debounce } from "@core/utils";
-import { normalizeInitiaState } from "@/redux/initialState";
+import { Page } from '@core/Page'
+import { Excel } from '@/components/excel/Excel'
+import { Header } from '@/components/header/Header'
+import { Formula } from '@/components/formula/Formula'
+import { Table } from '@/components/table/Table'
+import { Toolbar } from '@/components/toolbar/Toolbar'
+import { createStore } from '@core/store/createStore'
+import { rootReducer } from '@/redux/rootReducer'
+import { storage, debounce } from '@core/utils'
+import { normalizeInitiaState } from '@/redux/initialState'
 
 function storageName(param) {
-  return "excel:" + param;
+  return 'excel:' + param
+}
+
+class StateProcessor {
+  constructor(client, delay = 300) {
+    this.client = client
+    this.listen = debounce(this.listen.bind(this), delay)
+  }
+
+  listen(state) {
+    this.client.save(state)
+  }
+
+  get() {
+    return this.client.get()
+  }
+}
+
+class LocalStorageClient {
+  constructor(name) {
+    this.name = storageName(name)
+  }
+
+  save(state) {
+    storage(this.name, state)
+    return Promise.resolve()
+  }
+
+  get() {
+    // return Promise.resolve(storage(this.name))
+    return new Promise((resolve) => {
+      const state = storage(this.name)
+      setTimeout(() => {
+        resolve(state)
+      }, 700)
+    })
+  }
 }
 
 export class ExcelPage extends Page {
-  getRoot() {
-    const params = this.params ? this.params : Date.now().toString();
-    const state = storage(storageName(params));
-    const store = createStore(rootReducer, normalizeInitiaState(state));
-    const stateListener = debounce((state) => {
-      storage(storageName(params), state);
-    }, 300);
+  constructor(params) {
+    super(params)
 
-    store.subscribe(stateListener);
+    this.storeSub = null
+    this.processor = new StateProcessor(new LocalStorageClient(this.params))
+  }
+  async getRoot() {
+    const state = await this.processor.get()
+    const initialState = normalizeInitiaState(state)
+    const store = createStore(rootReducer, initialState)
+
+    this.storeSub = store.subscribe(this.processor.listen)
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
       store,
-    });
-    return this.excel.getRoot();
+    })
+    return this.excel.getRoot()
   }
   afterRender() {
-    this.excel.init();
+    this.excel.init()
   }
   destroy() {
-    this.excel.destroy();
+    this.excel.destroy()
+    this.storeSub.unsubscribe()
   }
 }
